b900ee53c19e3ff8e31dad04e5d1b3c2bde5ca3d
sylvainvant <sylvainvantieghem@gmail.com>
2018-12-28 03:29:48 +0100

    first commit

---
+++ fiche1/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace fiche1
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormPrincipal));
+            this.label1 = new System.Windows.Forms.Label();
+            this.lb_liste1 = new System.Windows.Forms.ListBox();
+            this.lb_list2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "un petit exo sympa...\r\n";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lb_liste1
+            // 
+            this.lb_liste1.FormattingEnabled = true;
+            this.lb_liste1.Location = new System.Drawing.Point(102, 39);
+            this.lb_liste1.Name = "lb_liste1";
+            this.lb_liste1.Size = new System.Drawing.Size(214, 264);
+            this.lb_liste1.Sorted = true;
+            this.lb_liste1.TabIndex = 1;
+            // 
+            // lb_list2
+            // 
+            this.lb_list2.FormattingEnabled = true;
+            this.lb_list2.Location = new System.Drawing.Point(421, 39);
+            this.lb_list2.Name = "lb_list2";
+            this.lb_list2.Size = new System.Drawing.Size(216, 264);
+            this.lb_list2.Sorted = true;
+            this.lb_list2.TabIndex = 2;
+            this.lb_list2.SelectedIndexChanged += new System.EventHandler(this.lb_list2_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(351, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(55, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = ">>\r\n";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(351, 109);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(55, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "<<";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button3.Location = new System.Drawing.Point(128, 368);
+            this.button3.Name = "button3";
+            this.button3.Padding = new System.Windows.Forms.Padding(2);
+            this.button3.Size = new System.Drawing.Size(57, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "+";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(237, 368);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(55, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "-";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(446, 368);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(55, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "+";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(553, 368);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(54, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "-";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(344, 410);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "quitter";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(102, 328);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(214, 20);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.TextChanged += new System.EventHandler(this.tb_saissie_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(421, 328);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(216, 20);
+            this.textBox2.TabIndex = 11;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(751, 474);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lb_list2);
+            this.Controls.Add(this.lb_liste1);
+            this.Controls.Add(this.label1);
+            this.Name = "FormPrincipal";
+            this.Padding = new System.Windows.Forms.Padding(1);
+            this.Text = "deux listes";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lb_liste1;
+        private System.Windows.Forms.ListBox lb_list2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ fiche1/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fiche1
+{
+    public partial class FormPrincipal : Form
+    {
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lb_list2.Items.AddRange(lb_liste1.Items);
+            lb_liste1.Items.Clear();
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text =="")
+            {
+                MessageBox.Show("La zone de saisie est vide");
+            }
+            else
+            {
+                lb_liste1.Items.Add(textBox1.Text);
+                textBox1.Text = "";	// ou tb_saisie.Clear()
+                textBox1.Focus();
+            }
+        }
+
+        private void tb_saissie_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text =="")
+            {
+                MessageBox.Show("La zone de saisie est vide");
+            }
+            else
+            {
+                lb_list2.Items.Add(textBox2.Text);
+                textBox2.Text = "";	// ou tb_saisie.Clear()
+                textBox2.Focus();
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (lb_liste1.Text == "")
+            {
+                MessageBox.Show("Vous avez cliquer sur '-' sans item sélectionné dans la liste");
+            }
+            else
+            {
+                lb_liste1.Items.RemoveAt(lb_liste1.SelectedIndex);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (lb_list2.Text == "")
+            {
+                MessageBox.Show("Vous avez cliquer sur '-'sans item sélectionné dans la liste");
+            }
+            else
+            {
+                lb_list2.Items.RemoveAt(lb_list2.SelectedIndex);
+            }
+        }
+
+        private void lb_list2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Quitter l'application?", "Message de confirmation", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                Application.Exit();
+
+     
+          
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lb_liste1.Items.AddRange(lb_list2.Items);
+            lb_list2.Items.Clear();
+        }
+     
+
+
+
+
+      
+            
+           
+        
+    }
+}
